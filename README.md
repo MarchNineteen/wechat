02806fdd56f8f3fc2b022b2adebcfe8cc04a7201
kunzite <1501987783@qq.com>
2018-05-09 14:44:40 +0800

    初始化结构

---
+++ README.md
@@ -0,0 +1,3 @@
+# 微信demo
+### accout 公众号
+### program 小程序
---
+++ wechat-accout/src/main/java/com/wyb/accout/WechatAccoutApplication.java
@@ -0,0 +1,12 @@
+package com.wyb.accout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WechatAccoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WechatAccoutApplication.class, args);
+	}
+}
